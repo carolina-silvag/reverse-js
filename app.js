6c2e4f46b1289f76145b9631915e676413ca1471
carolina-silvag <carolinasguzman@gmail.com>
2017-09-27 21:33:42 -0300

    solucion para ejercicio uno Reverse

---
+++ app.js
@@ -0,0 +1,11 @@
+var listOfNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function reverse(listOfNumber){
+	var newList =[]
+	for (var i = listOfNumber.length; i-1>= 0; i--){
+		newList.push(listOfNumber[i-1])
+	}
+	return newList
+}
+
+console.log(reverse(listOfNumber))
